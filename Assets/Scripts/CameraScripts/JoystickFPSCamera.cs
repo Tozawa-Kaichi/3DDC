903d4af0f681d0b1b6182d0a8c9fec0c689e1ae9
st37071556@icloud.com <st37071556@icloud.com>
2023-03-24 22:28:01 +0900

    moveCameraByJoystick

---
+++ Assets/Scripts/CameraScripts/JoystickFPSCamera.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickFPSCamera : MonoBehaviour
+{
+    [SerializeField] FixedJoystick _joyStick;
+    [SerializeField] float _cameraMoveSpeed;
+    Vector3 _cameraAngle;
+    private void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+    void RotateByJoystick()
+    {
+
+    }
+}
