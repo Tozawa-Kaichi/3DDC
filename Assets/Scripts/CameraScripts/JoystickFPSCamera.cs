d1a2ff270af0b4a227364b7d462ed3b2996413c5
st37071556@icloud.com <st37071556@icloud.com>
2023-03-27 23:17:37 +0900

    Clampで制限を試してみる

--- Assets/Scripts/CameraScripts/JoystickFPSCamera.cs
+++ Assets/Scripts/CameraScripts/JoystickFPSCamera.cs
@@ -6,6 +6,9 @@ public class JoystickFPSCamera : MonoBehaviour
 {
     [SerializeField] FixedJoystick _joyStick;
     [SerializeField] float _cameraMoveSpeed;
+    [SerializeField] float _limitAngleMaxUP;
+    [SerializeField] float _limitAngleMinDown;
+
     Vector3 _cameraAngle;
     private void Update()
     {
@@ -13,10 +16,12 @@ public class JoystickFPSCamera : MonoBehaviour
             _joyStick.Horizontal * _cameraMoveSpeed,
             _joyStick.Vertical * _cameraMoveSpeed,
             0);
+
     }
     private void FixedUpdate()
     {
-        transform.eulerAngles += new Vector3(-_cameraAngle.y, _cameraAngle.x);
+        transform.eulerAngles += new Vector3(
+            Mathf.Clamp(_cameraAngle.y, _limitAngleMinDown, _limitAngleMaxUP) , _cameraAngle.x);
     }
     void RotateByJoystick()
     {
